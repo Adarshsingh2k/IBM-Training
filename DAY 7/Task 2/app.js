5dedb56869f725e2ff8074ab483b378140958340
Adarsh Singh <adarshsingh2k@gmail.com>
2022-02-02 12:46:45 +0530

    Day 7-> task 2 updated

---
+++ DAY 7/Task 2/app.js
@@ -0,0 +1,21 @@
+const stud={
+    firstName:"adarsh",
+    lastName:"Singh",
+    age: 20
+};
+
+// to get keys=> key
+// to get values use student[key]
+for(key in stud){
+    console.log(key);
+    
+}
+for(key in stud){
+    console.log(stud[key]);
+    
+}
+
+stud.class=10;
+console.log(stud);
+delete stud.class;
+console.log(stud);
