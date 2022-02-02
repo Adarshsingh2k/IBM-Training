6aa62dbee5a42067d097b907826b9e23f4666b3f
Adarsh Singh <adarshsingh2k@gmail.com>
2022-02-02 15:31:52 +0530

    Day 7->Task 3 [Restructuring, Rest param, Spread operator]

--- DAY 7/Task 3/app.js
+++ DAY 7/Task 3/app.js
@@ -12,3 +12,22 @@ function sum(...args){
 const numbers = [1, 2, 3,4,5,6];
 
 console.log(sum(...numbers));
+
+
+// Destructuring
+// 1.Array
+let newArr=[1,2,3,4,5,6];
+let [x,,,y,,z]=newArr;
+console.log(x,y,z);
+
+// 2. Objects --> need to see once again
+const stud={
+    firstN:"adarsh",
+    lastN:"Singh",
+} ;
+
+let{firstN:fname,lastN:lname}=stud;
+console.log(fname,lname);
+// with default values
+let{firstN,lastN,age=40}=stud;
+console.log(firstN,lastN,age);
